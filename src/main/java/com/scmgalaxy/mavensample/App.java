ca9fa3dc60df4f33ef72c558f580a37d1b7e284a
Niharika1909 <niharika1909@gmail.com>
2020-10-29 13:56:26 +0530

    Update App.java

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World RefX! MAH-2 test" );
+        System.out.println( "Hello World Test" );
     }
 }
